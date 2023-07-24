58a05b5669b9d4c7d2e7faca3a71934fe1ff169f
Sumayah07 <muhydeensumayah27@gmail.com>
2023-07-24 16:04:58 -0700

    handle.c

--- handle.c
+++ handle.c
@@ -1,107 +1,50 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-int _printf(const char *format, ...)
+#include "main.h"
+/**
+ * ev_print_func - returns the amount of identifiers.
+ * @s: argument indentifier
+ * @index: index of argument identifier.
+ * Return: amount of identifiers.
+ */
+int ev_print_func(const char *s, int index)
 {
-	va_list args;
-	int count = 0;
+	print_t pr[] = {
+		{"c", print_chr}, {"s", print_str}, {"i", print_int},
+		{"d", print_int}, {"b", print_bnr}, {"u", print_unt},
+		{"o", print_oct}, {"x", print_hex}, {"X", print_upx},
+		{"S", print_usr}, {"p", print_add}, {"li", prinlint},
+		{"ld", prinlint}, {"lu", prinlunt}, {"lo", prinloct},
+		{"lx", prinlhex}, {"lX", prinlupx}, {"hi", prinhint},
+		{"hd", prinhint}, {"hu", prinhunt}, {"ho", prinhoct},
+		{"hx", prinhhex}, {"hX", prinhupx}, {"#o", prinnoct},
+		{"#x", prinnhex}, {"#X", prinnupx}, {"#i", print_int},
+		{"#d", print_int}, {"#u", print_unt}, {"+i", prinpint},
+		{"+d", prinpint}, {"+u", print_unt}, {"+o", print_oct},
+		{"+x", print_hex}, {"+X", print_upx}, {" i", prinsint},
+		{" d", prinsint}, {" u", print_unt}, {" o", print_oct},
+		{" x", print_hex}, {" X", print_upx}, {"R", print_rot},
+		{"r", print_rev}, {"%", print_prg}, {"l", print_prg},
+		{"h", print_prg}, {" +i", prinpint}, {" +d", prinpint},
+		{"+ i", prinpint}, {"+ d", prinpint}, {" %", print_prg},
+		{NULL, NULL},
+	};
+	int i = 0, j = 0, first_index;
 
-	va_start(args, format);
-
-	while (*format != '\0')
+	first_index = index;
+	while (pr[i].type_arg)
 	{
-		if (*format == '%')
+		if (s[index] == pr[i].type_arg[j])
 		{
-			format++;
-			/*Move past the '%'
-			 * Handle the conversion specifier*/
-			switch (*format)
-			{
-				case 'c':
-					{
-						char c = va_arg(args, int);
-						write(1, &c, 1);
-						/*Write the character to stdout*/
-						count++;
-						break;
-					}
-				case 's':
-					{
-						char *str = va_arg(args, char *);
-						int i = 0;
-						while (str[i] != '\0')
-						{
-							write(1, &str[i], 1);
-							/*Write each character of the string to stdout*/
-							i++;
-							count++;
-						}
-						break;
-					}
-				case '%':
-					{
-						write(1, "%", 1);
-						/*Write a single '%' character to stdout*/
-						count++;
-						break;
-					}
-				case 'd':
-				case 'i':
-					{
-						int num = va_arg(args, int);
-						int length = 0;
-						int temp = num;
-						/*Calculate the length of the number*/
-						do {
-							length++;
-							temp /= 10;
-						} while (temp != 0);
-
-						/*Handle negative numbers*/
-						if (num < 0)
-						{
-							write(1, "-", 1);
-							/*Write the negative sign*/
-							count++;
-							num = -num;
-							/*Convert the number to positive*/
-						}
-						/*
-						 * Write each digit of the number to stdout*/
-						for (int i = length - 1; i >= 0; i--)
-						{
-							char digit = '0' + (num / power(10, i) % 10);
-							write(1, &digit, 1);
-							count++;
-						}
-						break;
-					}
-				default:
-					write(1, "%", 1);
-					/*Write the '%' character if an unsupported format specifier is encountered*/
-					count++;
-					break;
-			}
+			if (pr[i].type_arg[j + 1] != '\0')
+				index++, j++;
+			else
+				break;
 		}
 		else
 		{
-			write(1, format, 1);
-			/*Write any other character to stdout*/
-			count++;
+			j = 0;
+			i++;
+			index = first_index;
 		}
-		format++;
 	}
-	va_end(args);
-	return "count;"
-}
-/*Helper function to calculate the power of a number*/
-int power(int base, int exponent)
-{
-	int result = 1;
-
-	for (int i = 0; i < exponent; i++)
-	{
-		result *= base;
-	}
-	return result();
+	return (j);
 }
