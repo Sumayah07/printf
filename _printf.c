955465217f7bdc5b300a5332f7b398a52e8c053b
Sumayah07 <muhydeensumayah27@gmail.com>
2023-07-25 13:34:20 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,67 +1,51 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-/*
- * _printf - write output to stdout, the standard output stream
- * Returns: the number of characters printed (excluding the null byte used to end output to strings)
+#include "main.h"
+
+/**
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-	va_start(args, format);
-
-	while (*format != '\0')
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
 	{
-		if (*format == '%')
+		if (format[i] == '%')
 		{
-			format++;
-
-			/*Move past the '%'
-			 * Handle the conversion specifier
-			 */
-			switch (*format)
-			{
-				case 'c':
-					{
-						char c = va_arg(args, int);
-						write(1, &c, 1);
-						/*Write the character to stdout*/
-						count++;
-						break;
-					}
-				case 's':
-					{
-						char *str = va_arg(args, char *);
-						int i = 0;
-						while (str[i] != '\0')
-						{
-							write(1, &str[i], 1);/*Write each character of the string to stdout*/
-							i++;
-							count++;
-						}
-						break;
-					}
-				case '%':
-					{
-						write(1, "%", 1);/*Write a single '%' character to stdout*/
-						count++;
-						break;
-					}
-				default:
-					write(1, "%", 1);  /*Write the '%' character if an unsupported format specifier is encountered*/
-					count++;
-					break;
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
 			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
 		}
 		else
-		{
-			write(1, format, 1);/*Write any other character to stdout*/
-			count++;
-		}
-		format++;
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-	va_end(args);
-	return count(0);
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
