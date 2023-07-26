b23b616baf77bf770d714c6da77ebb8882e432c7
Andrewoflagos1 <andrewmalary0@gmail.com>
2023-07-26 15:06:02 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,65 +1,66 @@
-<<<<<<< HEAD
+#include "main.h"
 
-=======
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-	while (*format != '\0')
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (*format == '%')
+		if (format[i] != '%')
 		{
-			format++;
-			/*Move past the '%'
-			 * Handle the conversion specifier
-			 */
-			switch (*format)
-			{
-				case 'c':
-					{
-						char c = va_arg(args, int);
-						write(1, &c, 1);/*Write the character to stdout*/
-						count++;
-						break;
-					}
-				case 's':
-					{
-						char *str = va_arg(args, char *);
-						int i = 0;
-						while (str[i] != '\0')
-						{
-							write(1, &str[i], 1);/*Write each character of the string to stdout*/
-							i++;
-							count++;
-						}
-						break;
-					}
-				case '%':
-					{
-						write(1, "%", 1);/*Write a single '%' character to stdout*/
-						count++;
-						break;
-					}
-				default:
-					write(1, "%", 1);  /*Write the '%' character if an unsupported format specifier is encountered*/
-					count++;
-					break;
-			}
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
-			write(1, format, 1);/*Write any other character to stdout*/
-			count++;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
-		format++;
 	}
-	va_end(args);
-	return count;
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
->>>>>>> d1e7b59fb46e5ec41105cddcf739ade3dd1a03ed
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
